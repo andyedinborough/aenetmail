fc396f48588c506bde710223cdbd03ecc88251a9
Andy Edinborough <andy@edinborough.org>
2013-01-20 10:14:42 -0600

    Fix for index out of range during DecodeQuoatedPrintable; closes #117

--- Utilities.cs
+++ Utilities.cs
@@ -164,7 +164,7 @@ namespace AE.Net.Mail {
 			for (int i = 0; i < data.Length; i++) {
 				var b = data[i];
 
-				if ((b == eq) && ((i + 1) < data.Length)) {
+				if ((b == eq) && ((i + 2) < data.Length)) {
 					byte b1 = data[i + 1], b2 = data[i + 2];
 					if (b1 == 10 || b1 == 13) {
 						i++;
@@ -302,11 +302,11 @@ namespace AE.Net.Mail {
 		private const char Base64Padding = '=';
 
 		private static readonly HashSet<char> Base64Characters = new HashSet<char>() { 
-            'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 
-            'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 
-            'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 
-            'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '+', '/'
-        };
+						'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 
+						'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 
+						'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 
+						'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '+', '/'
+				};
 
 		internal static bool IsValidBase64String(string param) {
 			if (param == null) {
