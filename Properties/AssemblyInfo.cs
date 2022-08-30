296fe0ddd5fc4cf33d9b51ca1a46abb9c5f6d528
WANG,Youtian <wangyoutian@nilnul.com>
2022-08-30 12:11:33 +0800

    modified .csproj for automation of devOp; added /Properties/ReadThis.cs(which describes where the .snk blob is); etc.

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -6,12 +6,12 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("AE.Net.Mail")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("This is a fork of Andy Edinborough's project. To avoid name conflicts, and to apply our devOp automation, we append a prefix, with full respect for the original author")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Andy Edinborough")]
 [assembly: AssemblyProduct("AE.Net.Mail")]
-[assembly: AssemblyCopyright("")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCopyright("Original Author and All Contributors")]
+[assembly: AssemblyTrademark("AE.Net.Mail by Andy Edinborough")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
@@ -32,6 +32,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.7.*")]
+[assembly: AssemblyVersion("1.8")]  // keep the version stable, as even the build part of the version change will make the winform designer fail to render controls. Rather, we can change the nuget package version instead.
 [assembly: AssemblyFileVersion("1.7.10.0")]
 //[assembly: InternalsVisibleTo("Tests")]
