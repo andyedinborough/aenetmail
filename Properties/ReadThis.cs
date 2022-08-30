0a96d7ee27e073b35c5814c6d57d78f31341722d
WANG,Youtian <wangyoutian@nilnul.com>
2022-08-30 18:20:45 +0800

    Updated Properties/ReadThis.cs

--- Properties/ReadThis.cs
+++ Properties/ReadThis.cs
@@ -2,14 +2,14 @@
 ///		-conservative for this. It seems if it targets 4.8, it's fine with 4.8.1
 ///
 
-///This is a fork of Andy Edinborough's project. To avoid name conflicts, and to apply our devOp automation, we append a prefix. If you find this project helpful, please join us in being thankful to the original author.
+///This is a fork of Andy Edinborough's project. To avoid name conflicts, and to apply our devOp automation, we prepend a prefix to the repo path/name. If you find this project helpful, please join us in being thankful to the original author.
 /// <see cref="AssemblyInfo.cs"/> for the upstream project information.
 /// 
 /// as we have made changes to the project's meta (the .csproj file itself), it follows nilnul devOp pipeline, and it might be or not be merged well with other forks. As how fork works, you can merge here, there, or create your own fork.
 
 /// as strong-named referencers need strong-named referencees, and also strong name is painful and adds no value, hence we use an arbitrary ".snk" file.
 /// all our projects share the same .snk file. So we made it a symlink, pointed by a local doc inside each project. The real .snk file can be found in git repo:
-///		nilnul.dev._src_.eg_.nilnul0
+///		nilnul.dev._src_.eg_.main
 ///			which can be found in many public git services including:
-///				https://github.com/nilnul/nilnul.dev._src_.eg_.nilnul0
+///				https://github.com/nilnul/nilnul.dev._src_.eg_.main
 /// This makes our intention and methodology more obvious, and you can easily change it to your own; but we recommend keeping it, as we have previously said: its value doesnot deserve much work, and it's painful as all referencers will break if you change the strong-name.
